6f57906808cccaf21a4bbea3ef259c76988c94f2
Fenno Boomgaarden <fenno.boomgaarden@t-online.de>
2018-08-07 19:14:43 +0200

    added missing files

---
+++ Hexasprout/Assets/Resources/Scripts/ButtonScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GUI_Event ButtonID;
+    public GameObject gui;
+
+    // Called, when button clicked
+    public void HandleClick()
+    {
+        gui.GetComponent<GUIManager>().EventHandler(ButtonID);
+    }
+    
+    // Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
