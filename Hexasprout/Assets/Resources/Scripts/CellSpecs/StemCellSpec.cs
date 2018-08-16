f2889f35cc8aba3cac19ccdbbf1a0c5797695096
Benjamin Stahr <BenjaminStahr97@web.de>
2018-08-16 17:38:02 +0200

    Allen Zellen wurde ein Buildmanager hinzugefügt, welcher Buildaufträge entgegennehmen kann und den Abschluss derer den Zellen mitteilen kann

--- Hexasprout/Assets/Resources/Scripts/CellSpecs/StemCellSpec.cs
+++ Hexasprout/Assets/Resources/Scripts/CellSpecs/StemCellSpec.cs
@@ -7,10 +7,18 @@ public class StemCellSpec : MonoBehaviour {
     public FieldManager Field;
     public CellManager CellManager;
 
+    public FieldManager buildTarget;
+    public string buildName;
+    public int positionOfBuildTarget;
+    public BuildManager BuildManager;
+
+
+
     private Animator[] animator;
    
     private void Awake()
     {
+        BuildManager = this.gameObject.GetComponentInParent<BuildManager>();
         CellManager = this.gameObject.GetComponentInParent<CellManager>();
         animator = new Animator[6];
 
@@ -40,33 +48,78 @@ public class StemCellSpec : MonoBehaviour {
         switch (e)
         {
             case GUI_Event.Grow:
-                BuildConnection();
+                StartBuild();
+                break;
+            case GUI_Event.BuildReady:
+                FinishBuild(buildName);
                 break;
         }
     }
 
-    void BuildConnection()
+    void StartBuild()
     {
         for (int i = 0; i < Field.neighbours.Length; i++)
         {
             if (Field.neighbours[i] != null)
             {
                 if (Field.neighbours[i].State == FieldState.SuperSelected && !Field.neighbours[i].HasMaterial())
-                {                
+                {
+                    buildTarget = Field.neighbours[i];
+                    positionOfBuildTarget = i;
+
                     if (Field.neighbours[i].Cell == null)
-                    {
-                        GameObject.Find("World").GetComponent<WorldGenerator>().CreateStemCell(Field.neighbours[i]);
+                    {    
+                        OrderNewCell();
                     }
-                    Field.Cell.GetComponent<CellManager>().ConnectWith(Field.neighbours[i].Cell.GetComponent<CellManager>(), i);
-                    animator[i].SetTrigger("Start");
+                    else
+                    {
+                        OrderNewConnection();
+                    }
                 }
             }
         }
     }
-    
-    // Update is called once per frame
-    public void OwnFixedUpdate()
-    {
 
+    void FinishBuild(string type)
+    {
+        switch(type)
+        {
+            case "Build Cell":
+                MakeCell();
+                break;
+            case "Build Connection":
+                MakeConnection();
+                break;
+        }
     }
+    //Here are the buildmethods, for one build are always a order and a make method necessary
+
+    void OrderNewCell()
+    {
+        buildName = "Build Cell";
+        //first parameter is time in seconds, second the required juice, third the Name of the Buildevent
+        this.gameObject.GetComponent<BuildManager>().Build(10, new Juice(0f,0f,0f,0f,0f,1f), buildName);
+    }
+
+    void OrderNewConnection()
+    {
+        buildName = "Build Connection";
+        this.gameObject.GetComponent<BuildManager>().Build(10, new Juice(0f, 0f, 0f, 0f, 0f, 0.2f), buildName);
+    }
+
+    void MakeCell()
+    {
+        GameObject.Find("World").GetComponent<WorldGenerator>().CreateStemCell(buildTarget);
+        MakeConnection();
+    }
+
+    void MakeConnection()
+    {
+        Field.Cell.GetComponent<CellManager>().ConnectWith(buildTarget.Cell.GetComponent<CellManager>(), positionOfBuildTarget);
+        animator[positionOfBuildTarget].SetTrigger("Start");
+
+        buildTarget = null;
+        positionOfBuildTarget = 0;
+    }
+    public void OwnFixedUpdate(){}
 }
